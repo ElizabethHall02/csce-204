cc33e4605ccfb78f5922e1d5873735a4eb5b39dc
ElizabethHall02 <77406513+ElizabethHall02@users.noreply.github.com>
2022-09-08 10:32:54 -0400

    Add files via upload

---
+++ assignment06/zodiac_signs.py
@@ -0,0 +1,52 @@
+month = (input("Enter your birthmonth: "))
+day = int(input("Enter your birthday:" ))
+
+if month == "december" and day >=22:
+    print("Capricorn: Ruby")
+elif month == "january" and day <=21:
+    print("capricorn: Ruby")
+elif month == "january" and day >=21:
+    print("Aquarius: Garnet")
+elif month == "february" and day <=21:
+    print("Aquarius: Garnet")
+elif month == "february" and day >=22:
+    print("Pisces: Amethyst")
+elif month == "march" and day <=21:
+    print("Pisces: Amethyst")
+elif month == "march" and day >=21:
+    print("Aries: Bloodstone")
+elif month == "april" and day <=20:
+    print("Aries: Bloodstone")
+elif month == "april" and day >=21:
+    print("Taurus: Sapphire")
+elif month == "may" and day <=21:
+    print("Taurus: Sapphire")
+elif month == "may" and day >=22:
+    print("Gemini: Agate")
+elif month == "june" and day <=21:
+    print("Gemini: Agate")
+elif month == "june" and day >=22:
+    print("Cancer: Emerald")
+elif month == "july" and day <=22:
+    print("Cancer: Emerald")
+elif month == "july" and day >=23:
+    print("Leo: Onyx")
+elif month == "august" and day <=22:
+   print("Leo: Onyx")
+elif month == "august" and day >=23:
+    print("Virgo: Carnelian")
+elif month == "september" and day <=22:
+    print("Virgo: Carnelian")
+elif month == "september" and day >=23:
+    print("Libra: Peridot")
+elif month == "october" and day <=23:
+    print("Libra: Peridot")
+elif month == "october" and day >=24:
+    print("Scorpio: Emerald or Aquamarine")
+elif month == "november" and day <=21:
+    print("Scorpio: Emerald or Aquamarine")
+elif month == "november" and day >=22:
+    print("Sagittarius: Topaz")
+elif month == "december" and day <=21:
+    print("Sagittarius: Topaz")
+
